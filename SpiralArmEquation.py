bb96233b3b9f8af8dfd78b8a4035c4f2660a09b7
Oash0739 <62128852+Oash0739@users.noreply.github.com>
2021-02-10 12:28:03 +1100

    Add files via upload

--- SpiralArmEquation.py
+++ SpiralArmEquation.py
@@ -6,11 +6,10 @@ from matplotlib.animation import PillowWriter
 from mpl_toolkits.mplot3d import Axes3D
 
 #plotting stuff
-fig = plt.figure()
-fig.set_dpi(100)
+fig, ax = plt.subplots(2, 1, constrained_layout=True,figsize=(15,15))
+fig.set_dpi(200)
 # ax = Axes3D(fig)
-ax1 = fig.add_subplot(1,1,1)
-# ax2 = fig.add_subplot(2,1,1)
+
 
 '''
 All the equations needed
@@ -19,6 +18,7 @@ All the equations needed
 omega = v_phi/R 
 kappa = np.sqrt(R(d omega^2/d R)+4omega^2)
         d/dR(omega^2) = (-2*v_phi^2)/R^3
+nu = (\partial^2 Phi)/(\partial z^2)
 omega_d = omega - 0.5*kappa
 phi = (omega_d+omega_0)*(T/978)+phi_D0
 phi_D0 is an arbitrary offset (moves phi_d up and down)
@@ -27,12 +27,13 @@ phi_d0 = 0
 no_time_steps = 1000
 T = np.linspace(0,1000,no_time_steps)
 no_bodies = 1000
-maxR=10
+maxR=20
 R = np.linspace(1,maxR,no_bodies)
 v_phi = 100/R 
 
 omega_list = []
 omega_d_list = []
+omega_b_list = []
 
 for i in range(no_bodies):
     omega = v_phi[i]/R[i]
@@ -42,56 +43,64 @@ for i in range(no_bodies):
     kappa1 = (-2*v_phi[i]**2)/R[i]**2  
     kappa = np.sqrt(kappa1+4*omega**2)
 
+    # nu = 
+
     omega_d = omega-0.5*kappa
     omega_d_list.append(omega_d)
 
-phi_fast_list = []
-phi_slow_list=[]
+phi_d_plus_list = []
+phi_d_minus_list =[]
+phi_b_plus_list = []
+phi_b_minus_list=[]
+omega_time_list=[]
 for t in range(len(T)):
-    phi_fast=[]
-    phi_slow = []
+    phi_d_plus=[]
+    phi_d_minus=[]
+    phi_b_plus=[]
+    phi_b_minus = []
+    omega_time=[]
     for i in range(no_bodies):
-        phi_fast.append((omega_d_list[i]+omega_list[i])*T[t]/978+phi_d0)
-        phi_slow.append((omega_d_list[i]/2+omega_list[i])*T[t]/978+phi_d0)
-    phi_fast_list.append(phi_fast)
-    phi_slow_list.append(phi_slow)
+        phi_d_plus.append((omega_d_list[i]+omega_list[i])*T[t]/978+phi_d0)
+        phi_d_minus.append((omega_d_list[i]-omega_list[i])*T[t]/978+phi_d0)
+        phi_b_plus.append((omega_d_list[i]/2+omega_list[i])*T[t]/978+phi_d0)
+        phi_b_minus.append((omega_d_list[i]/2-omega_list[i])*T[t]/978+phi_d0)
+        omega_time.append(omega_list[i]*T[t]/978+phi_d0)
+    phi_d_plus_list.append(phi_d_plus)
+    phi_d_minus_list.append(phi_d_minus)
+    phi_b_plus_list.append(phi_b_plus)
+    phi_b_minus_list.append(phi_b_minus)
+    omega_time_list.append(omega_time)
 
-polar2z = lambda r,θ: r * np.exp( 1j * θ )
-z2polar = lambda z: ( np.abs(z), np.angle(z) )
-
-theta = np.linspace(0,2*pi,1000)
-r,th = np.meshgrid(R,theta)
-
-# z = polar2z(R,theta)
-# plt.imshow(np.real(z),np.imag(z),phi_fast_list[1])
-# plt.show()
-# for i in range(len(theta)):
-
-# print(phi_list)
 
 t = 0
 def animate(i):
     global t
-    ax1.clear()
-    ax1.plot(R,phi_slow_list[t],'.b-',label=r'Bending waves $\left(\frac{\Omega_d}{2}+\Omega\right)t$')
-    ax1.plot(R,phi_fast_list[t],'.r-',label = r'Density waves $\left(\Omega_d+\Omega\right)t$')
-    ax1.legend()  
-    plt.grid(True)
-    x = 't='
-    y = r'for $\phi_d=0$ '
-    ax1.set_title(f'{x}{t}')
-    ax1.set_xlabel('Radius')
-    ax1.set_ylabel(r'$\phi_d(R,t)$')
-    ax1.set_ylim(0,150)
+    ax[0].clear()
+    ax[1].clear()
+    ax[0].plot(R,phi_b_plus_list[t],label=r' $\left(\frac{\Omega_d}{2}+\Omega\right)t$',linewidth = 0.1,linestyle = 'dashdot',color = 'blue')
+    ax[0].plot(R,omega_time_list[t],'k',label = r'$\Omega',linewidth=0.1)
+    ax[0].plot(R,phi_b_minus_list[t],label=r' $\left(\frac{\Omega_d}{2}-\Omega\right)t$',linewidth=0.1,linestyle = 'dashed',color = 'red')
+    ax[1].plot(R,phi_d_plus_list[t],label=r'$\left(\Omega_d+\Omega\right)t$',linewidth=0.10,linestyle = 'dashdot',color = 'blue')
+    ax[1].plot(R,omega_time_list[t],'k',label = r'$\Omega',linewidth=0.1)
+    ax[1].plot(R,phi_d_minus_list[t],label = r'$\left(\Omega_d-\Omega\right)t$',linewidth=0.1,linestyle = 'dashed',color = 'red')
+    ax[0].legend()  
+    ax[1].legend()
+    x='t='
+    fig.suptitle(f'{x}{t}')
+    ax[0].set_title('Bending waves')
+    ax[1].set_title('Density waves')
+    ax[0].set_xlabel('Radius')
+    ax[0].set_ylabel(r'$\phi_d(R,t)$')
+    ax[0].set_ylim(0,max(phi_d_plus_list[-1]))
+    ax[1].set_ylim(0,max(phi_d_plus_list[-1]))
     # ax1.set_xlim(-maxR,maxR)
     t += 1
 anim = animation.FuncAnimation(fig,animate,frames=360,interval=20)
-# print(len(phi_fast_list[0]))
-# plt.subplot(projection="polar")
-# plt.pcolormesh(theta,R,phi_fast_list[0])
-# plt.show()
-writer = PillowWriter(fps=20, codec='libx264', bitrate=2)  
-anim.save("C:/Users/oasht/Dropbox/My PC (DESKTOP-TKRDL9R)/Documents/Uni/Honours/Project/GIFS/PHI_Equations.gif",writer = writer)
-
+plt.show()
+# writer = PillowWriter(fps=20, codec='libx264', bitrate=2) 
+# filelocatation = "C:/Users/oasht/Documents/Uni/honours/PROJECT/GIFS" 
+# filename = "PHI_Equations.gif"
+# filename = filelocatation+filename
+# anim.save(filename,writer = writer)
 
 
